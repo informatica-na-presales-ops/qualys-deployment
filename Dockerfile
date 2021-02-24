3cf6ad66502524816b37052fd3faa28bb8406544
William Jackson <william@subtlecoolness.com>
2021-02-24 11:43:04 -0600

    Add script to enumerate instances

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,8 @@ FROM python:3.9.2-alpine3.13
 COPY requirements.txt /qualys-deployment/requirements.txt
 RUN /usr/local/bin/pip install --no-cache-dir --requirement /qualys-deployment/requirements.txt
 
-ENV PYTHONUNBUFFERED="1" \
+ENV APP_VERSION="2021.0" \
+    PYTHONUNBUFFERED="1" \
     TZ="Etc/UTC"
+
+COPY deploy-to-aws.py /qualys-deployment/deploy-to-aws.py
---
+++ deploy-to-aws.py
@@ -0,0 +1,88 @@
+import apscheduler.schedulers.blocking
+import boto3
+import botocore.exceptions
+import logging
+import os
+import signal
+import sys
+
+log = logging.getLogger('qualys_deployment.deploy_to_aws')
+
+class Settings:
+    @staticmethod
+    def as_bool(value: str) -> bool:
+        return value.lower() in ('true', 'yes', 'on', '1')
+
+    @staticmethod
+    def as_int(value: str, default: int) -> int:
+        try:
+            return int(value)
+        except (ValueError, TypeError):
+            return default
+
+    @property
+    def log_format(self) -> str:
+        return os.getenv('LOG_FORMAT', '%(levelname)s [%(name)s] %(message)s')
+
+    @property
+    def log_level(self) -> str:
+        return os.getenv('LOG_LEVEL', 'INFO')
+
+    @property
+    def run_and_exit(self) -> bool:
+        return self.as_bool(os.getenv('RUN_AND_EXIT', 'false'))
+
+    @property
+    def run_interval(self) -> int:
+        # number of minutes between runs
+        # default run interval is 24 hours
+        return self.as_int(os.getenv('RUN_INTERVAL'), 60 * 24)
+
+    @property
+    def version(self) -> str:
+        return os.getenv('APP_VERSION', 'unknown')
+
+def get_instances(ec2):
+    filters = [
+        {
+            'Name': 'instance-state-name',
+            'Values': ['running']
+        }
+    ]
+    yield from ec2.instances.filter(Filters=filters)
+
+def main_job():
+    boto_session = boto3.session.Session()
+    for region in boto_session.get_available_regions('ec2'):
+        log.info(f'Checking {region}')
+        ec2 = boto3.resource('ec2', region_name=region)
+        try:
+            for instance in get_instances(ec2):
+                log.debug(f'Checking {instance.id} / {instance.platform} / {instance.key_name}')
+        except botocore.exceptions.ClientError as e:
+            log.critical(e)
+            log.critical(f'Skipping {region}')
+
+def main():
+    s = Settings()
+    logging.basicConfig(format=s.log_format, level=logging.DEBUG, stream=sys.stdout)
+    log.debug(f'{log.name} {s.version}')
+    if not s.log_level == 'DEBUG':
+        log.debug(f'Setting log level to {s.log_level}')
+    logging.getLogger().setLevel(s.log_level)
+
+    if s.run_and_exit:
+        main_job()
+        return
+
+    scheduler = apscheduler.schedulers.blocking.BlockingScheduler()
+    scheduler.add_job(main_job, 'interval', minutes=s.run_interval)
+    scheduler.add_job(main_job)
+    scheduler.start()
+
+def handle_sigterm(_signal, _frame):
+    sys.exit()
+
+if __name__ == '__main__':
+    signal.signal(signal.SIGTERM, handle_sigterm)
+    main()
